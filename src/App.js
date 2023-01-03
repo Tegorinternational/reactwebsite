ffb2b990af3f2f2eb293cf37deaf80d2fc7d6405
Tegor International <104412526+Tegorinternational@users.noreply.github.com>
2023-01-03 19:17:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import Header from './Components/Header';
+import Body from './Components/Body';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 function App() {
   return (
    <>
    <Header />
+   <Body />
    <Contact />
    <Footer />
    
